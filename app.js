3d332eb95650611340d04654ca3812d438e2be29
Bookaso <lohamat.s@gmail.com>
2021-02-16 14:58:44 +0700

    Add set Procfile gitignore and node version

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const app = express();
 
 app.set('view engine', 'ejs');
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://admin-bookaso:Test123>@cluster0.3bzbf.mongodb.net/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
@@ -84,6 +84,11 @@ app.get("/posts/:postName", function(req, res){
 
 });
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
